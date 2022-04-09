a01d30f233c502139b4942de8aa4f25f810764d9
HarshalShende <harshalshende357@gmail.com>
2022-04-09 12:00:42 +0530

    import export modules node js

--- exportodule.js
+++ exportodule.js
@@ -2,5 +2,14 @@ const add = (a, b) => {
     return a + b;
 };
 
+const sub = (a, b) => {
+    return a - b;
+};
+
 const name = "harshal";
-module.exports = add;
+
+// module.exports = name;
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+module.exports = { add, sub, name };
--- importmodule.js
+++ importmodule.js
@@ -1,3 +1,5 @@
-const add = require("./exportodule");
+const { add, sub, name } = require("./exportodule");
 
 console.log(add(5, 5));
+console.log(sub(10, 5));
+console.log(name);
