f91c8c059b4ad6540e8e8b4dd10ebf0565dba1fd
HarshalShende <harshalshende357@gmail.com>
2022-04-09 11:32:04 +0530

    path module

---
+++ pathmodule.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+console.log(path.dirname("G:/nodePractice/nodechallenge.js"));
+console.log(path.extname("G:/nodePractice/nodechallenge.js"));
+console.log(path.basename("G:/nodePractice/nodechallenge.js"));
+console.log(path.parse("G:/nodePractice/nodechallenge.js"));
