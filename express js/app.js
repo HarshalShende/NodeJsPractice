4047d9afc401fdf03e403bb271f4112daf9ef6b6
HarshalShende <harshalshende357@gmail.com>
2022-04-10 12:04:46 +0530

    express js code

--- express js/app.js
+++ express js/app.js
@@ -6,7 +6,28 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-    res.send("Hello From About Page");
+    res.write("Hello From About Page");
+    res.write("Hello From About Page");
+    res.send();
+});
+
+app.get("/contact", (req, res) => {
+    res.send({
+        id: 1,
+        name: "harshal",
+    });
+});
+
+app.get("/services", (req, res) => {
+    res.json([{
+            id: 1,
+            name: "harshal",
+        },
+        {
+            id: 2,
+            name: "sha",
+        },
+    ]);
 });
 
 app.listen(8000, () => {
